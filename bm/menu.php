f10b2d21ec471a620e77da0c41d0c6d053813a8a
Wintada <krit00007@hotmail.com>
2023-05-08 01:40:18 +0700

    new file bm

---
+++ bm/menu.php
@@ -0,0 +1,85 @@
+<?php
+session_start();
+?>
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-dark-warning elevation-4">
+  <!-- Brand Logo -->
+  <a href="index.php" class="brand-link">
+    <!-- <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8"> -->
+    <span class="brand-text  text-white">ระบบตรวจรับ RMUTT</span>
+  </a>
+
+
+  <!-- Sidebar -->
+  <div class="sidebar">
+    <!-- Sidebar user panel (optional) -->
+    <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+      <div class="image">
+      </div>
+      <div class="info">
+        <a href="index.php" class="d-block">
+          <?php echo $_SESSION['u_name'] . " ", $_SESSION['u_lastname']; ?> <br>
+          สถานะ <label class="h4 ml-1">
+            <?php echo $_SESSION['user_level']; ?>
+          </label>
+        </a>
+      </div>
+    </div>
+
+    <!-- Sidebar Menu -->
+    <nav class="mt-2">
+      <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+        <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+
+        <li class="nav-item">
+          <a href="index.php" data-toggle="dropdown" class="nav-link  <?php if ($menu == "index") {
+            echo "active";
+          } ?> ">
+            <i class="nav-icon fas fa-th-list"></i>
+            <p>รายการวัสดุและครุภัณฑ์</p>
+          </a>
+          <div class="dropdown-menu">
+            <a href="index.php?params=durableArticles" class="dropdown-item">ครุภัณฑ์</a>
+            <a href="index.php?params=material" class="dropdown-item ">วัสดุ</a>
+
+          </div>
+        </li>
+        <li class="nav-item">
+          <a href="report.php" class="nav-link <?php if ($menu == "report") {
+            echo "active";
+          } ?> ">
+            <i class="nav-icon fas fa-edit"></i>
+            <p>ตรวจสอบรายงาน</p>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="officer_location_status.php" class="nav-link <?php if ($menu == "officer_location_status") {
+            echo "active";
+          } ?> ">
+            <i class="nav-icon fas fa-edit"></i>
+            <p>เปลี่ยนสถานที่ครุภัณฑ์</p>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="officer_profile.php" class="nav-link <?php if ($menu == "officer_profile") {
+            echo "active";
+          } ?> ">
+            <i class="nav-icon fa fa-address-book"></i>
+            <p>จัดการข้อมูลส่วนตัว</p>
+          </a>
+        </li>
+
+        <li class="nav-item">
+          <a href="../logout.php" class="nav-link " onclick="return confirm('ยืนยันออกจากระบบ !!');">
+            <i class="nav-icon fas fa-sign-out-alt"></i>
+            <p class="text-white">ออกจากระบบ</p>
+          </a>
+        </li>
+
+      </ul>
+    </nav>
+    <!-- /.sidebar-menu -->
+  </div>
+  <!-- /.sidebar -->
+</aside>
---
+++ bm/report.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php $menu = "location_status"; ?>
+<?php include("head.php");
+
+?>
+
+<body class="hold-transition sidebar-mini layout-fixed">
+  <div class="wrapper">
+
+    <!-- Navbar -->
+    <?php include("navbar.php"); ?>
+    <!-- /.navbar -->
+    <?php include("menu.php"); ?>
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+
+      <!-- sweet aleart -->
+      <?php
+      if (@$_GET['do'] == 'success') {
+        echo '<script type="text/javascript">
+              swal("", "ทำรายการสำเร็จ !!", "success");
+              </script>';
+        // echo '<meta http-equiv="refresh" content="1;url=worker.php" />';
+      } else if (@$_GET['do'] == 'finish') {
+        echo '<script type="text/javascript">
+              swal("", "แก้ไขสำเร็จ !!", "success");
+              </script>';
+        // echo '<meta http-equiv="refresh" content="1;url=worker.php" />'; 
+      } else if (@$_GET['do'] == 'f') {
+        echo '<script type="text/javascript">
+              swal("", "ผิดพลาด !!", "error");
+              </script>';
+        // echo '<meta http-equiv="refresh" content="1;url=worker.php" />';
+      } ?>
+
+      <?php
+      include("condb.php"); // เชื่อมต่อฐานข้อมูล
+      
+      $result = $con->query("SELECT * FROM tbl_login ");
+      // $query_case = "SELECT * FROM di_data"
+      //  INNER JOIN tbl_login as u ON c.user_id = u.user_id
+      
+      // order by case_id asc" or die ("Error:" . mysqli_error());
+      // $result = mysqli_query($con, $query_case);
+      if (!$result) {
+        die('Error: ' . mysqli_error($con));
+      }
+      //   echo $query_case;
+//   exit();
+      ?>
+
+
+      <table id="example1" class="table table-bordered table-striped dataTable">
+        <thead>
+          <tr role="row" class="info">
+            <th tabindex="0" rowspan="1" colspan="1" style="width: 20%;">ชื่อ-นามสกุล</th>
+            <th tabindex="0" rowspan="1" colspan="1" style="width: 20%;">ไฟล์</th>
+            <th tabindex="0" rowspan="1" colspan="1" style="width: 20%;">ตรวจรับ</th>
+            <th tabindex="0" rowspan="1" colspan="1" style="width: 20%;">สถานะ</th>
+            <!-- <th  tabindex="0" rowspan="1" colspan="1" style="width: 10%;">วัน-เวลา</th> -->
+
+          </tr>
+        </thead>
+        <tbody>
+          <?php foreach ($result as $row) {
+            $i += 1 ?>
+            <tr>
+              <td>
+                <?php echo $row['u_name'], " ", $row['u_lastname']; ?>
+              </td>
+              <td>
+                <?php echo $row['DI_ID']; ?>
+              </td>
+              <td>
+                <div class="container">
+                  <?php echo $row['DI_STATUS']; ?> &nbsp;
+
+                  <!-- Trigger the modal with a button -->
+                  <button type="button" style="width:100px; height:50; font-size:10px;" class="btn btn-success"
+                    data-toggle="modal" data-target="#myModal">ตรวจรับ</button>
+
+                  <!-- Modal -->
+                  <div class="modal fade" id="myModal" role="dialog">
+                    <div class="modal-dialog">
+
+                      <!-- Modal content-->
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h4 class="modal-title">ตรวจรับ</h4>
+                          <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        </div>
+                        <div class="modal-body">
+                          <div class="container">
+                            <div class="row">
+                              <div class="col">
+                                <button type="submit" style="width:100px; height:50; font-size:10px;" method="post"
+                                  class="btn btn-success" onclick="return confirm('ยืนยันการแก้ไขข้อมูล !!');">ครบ
+                                
+                                </button>
+
+                              </div>
+                              <div class="col" align="right">
+                                <button type="submit" style="width:100px; height:50; font-size:10px;" method="post"
+                                  class="btn btn-danger"
+                                  onclick="return confirm('ยืนยันการแก้ไขข้อมูล !!');">ไม่ครบ</button>
+                              </div>
+                            </div>
+                          </div>
+
+                        </div>
+                      </div>
+
+                    </div>
+                  </div>
+
+                </div>
+              </td>
+              <td>
+                <?php echo $row['file_status']; ?>
+              </td>
+
+            <?php } ?>
+          </tr>
+        </tbody>
+      </table>
+
+      <!-- Main content -->
+
+    </div><!--content-wrapper  -->
+  </div><!--rapper  -->
+
+
+
+
+  <?php include("footer.php"); ?>
+
+  </div>
+  <!-- ./wrapper -->
+  <?php include("script.php"); ?>
+</body>
+
+</html>
